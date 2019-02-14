3014944d625b6016ec09b27ac21aae58771a4c59
Anup Khadka <akhadka.dev@gmail.com>
2019-02-14 00:03:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(divers) {
+  return divers.map(function(diver) {
+    return diver.toLowerCase();
+  });
+}
+
+function nameToAttributes(divers) {
+  return divers.map(function(diver) {
+    return Object.assign({}, {firstName: diver.split(" ")[0]}, {lastName: diver.split(" ")[1]});
+  });
+}
+
+function attributesToPhrase(divers) {
+    return divers.map(function(diver) {
+      return `${diver.name} is from ${diver.hometown}`;
+    });
+}

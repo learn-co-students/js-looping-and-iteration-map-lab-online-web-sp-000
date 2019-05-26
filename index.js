b48245b926ce70f470aa527ddd7e75c1877aa012
Rafael Batista <RafaelB24@yahoo.com>
2019-05-25 19:51:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function lowerCaseDrivers(drivers) {
-  const newDrivers = drivers;
-  return newDrivers.map(function(element) { return element.toLowerCase()});
+  //const newDrivers = drivers;
+  return drivers.map(function(element) { return element.toLowerCase()});
 }
 
 function nameToAttributes(drivers) {
-  const newDrivers = drivers;
-  return newDrivers.map(function(element) {return { firstName: element.split(' ')[0], lastName: element.split(' ')[1]} });
+  //const newDrivers = drivers;
+  return drivers.map(function(element) {return { firstName: element.split(' ')[0], lastName: element.split(' ')[1]} });
 }
 
 function attributesToPhrase(drivers) {
-  const newDrivers = drivers;
-  return newDrivers.map(function(element) {return `${element.name} is from ${element.hometown}`  });
+//  const newDrivers = drivers;
+  return drivers.map(function(element) {return `${element.name} is from ${element.hometown}`  });
 }

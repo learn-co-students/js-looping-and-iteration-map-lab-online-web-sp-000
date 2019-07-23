a05278ea8fe85ff228dd559b3312fb612a8e8a6c
github username <github email address>
2019-07-23 16:44:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers (drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  });
+};
+
+function nameToAttributes (drivers) {
+  newAttributes = {}
+  drivers.map(function(driver, newAttributes) {
+    const temp = driver.split(" ")
+    const key = temp[0]
+    const value = temp[1]
+    Object.assign(newAttributes, {key: value})
+  });
+  return newAttributes;
+};
+
+function attributesToPhrase () {
+
+};

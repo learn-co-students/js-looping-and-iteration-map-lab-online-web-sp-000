17d9453711cc36e9e7d562db3611a5b7feb53521
Ella <michaella.latkovic@gmail.com>
2019-08-21 19:50:05 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function(each){
+    return each.toLowerCase();
+  });
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(each){
+    const name = each.split(' ');
+    return { firstName: name[0], lastName: name[1] };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(each) {
+    return `${each.name} is from ${each.hometown}`;
+  });
+};

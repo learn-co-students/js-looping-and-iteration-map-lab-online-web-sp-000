da77f86a60045d5eb71eb34294aff2750988d854
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-22 03:20:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 // Code your solution in this file.
-function lowerCaseDrivers(driversArray) {
-  return driversArray.map(function(driverName) {
-    return driverName.toLowerCase()
+
+function lowerCaseDrivers(array) {
+  return array.map(function(element) {
+    return element.toLowerCase();
   })
 }
 
-function nameToAttributes(driversArray) {
-  return driversArray.map(function(fullName) {
-    return {firstName: fullName.split(" ")[0], lastName: fullName.split(" ")[1]}
+function nameToAttributes(array) {
+  return array.map(function(element) {
+    return {firstName: element.split(" ")[0], lastName: element.split(" ")[1]}
   })
 }
+// function nameToAttributes(array) {
+//   return array.map(function(element) {
+//     let newElement = {firstName: "", lastName: ""};
+//     let i = 0;
+//     while (i < element.length) {
+//     if (i < element.indexOf(" ")) {
+//       newElement.firstName = newElement.firstName + element[i];
+//     }
+//     if (i > element.indexOf(" ")) {
+//       newElement.lastName = newElement.lastName + element[i];
+//     }
+//     i++;
+//   } return newElement;
+//   });
+// }
 
-function attributesToPhrase(driversArray) {
-  return driversArray.map(function(object) {
-    return `${object.name} is from ${object.hometown}`
+function attributesToPhrase(array) {
+  return array.map(function(element) {
+    return `${element.name} is from ${element.hometown}`
   })
 }
-

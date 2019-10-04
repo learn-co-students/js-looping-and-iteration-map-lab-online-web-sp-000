d1aaab62072ee31eb4853ccfb7945329b6694e4c
Ramon Chavez <rachavez1974@gmail.com>
2019-10-03 23:30:26 -0400

    Finish js-looping-and-iteration-map-lab-online-web-sp-000

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  let  miniDrivers = drivers.map(function(name) {
+    return name.toLowerCase();
+  });
+  return miniDrivers;
+}
+
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(name) {
+    let wholeName = name.split(' ');
+    return {firstName: wholeName[0], lastName: wholeName[1]}
+  });
+  
+}
+
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
+
+
+// drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+// console.log(nameToAttributes(drivers));
